476071733af0e2141a21cd8b507f29ee819e16af
jaewapar <jaewan7567@naver.com>
2025-01-07 15:49:30 +0900

    tt

--- wan36GB.sh
+++ wan36GB.sh
@@ -1,86 +1,58 @@
 #!/bin/bash
 
-FINISH_EZ=0
-FINISH_LZ=1
-NOFINISH_EZ=2
-NOFINISH_LZ=3
+# 환경 변수 값 목록
+ZONE_VALUES=(5 10 20 30 64)
 
-OPTIONS=/home/jaewan/연구/YCSB-cpp/rocksdb/zlsm_motivation.ini
-#OPTIONS=/home/jaewan/연구/YCSB-cpp/rocksdb/zenfsoptions.ini
+# 실험 결과 경로
+RESULT_DIR_PATH=/home/jaewan/ATC_testdata/YCSB_zns/36GB
 
-RESULT_DIR_PATH=/home/jaewan/연구/ATC_testdata/YCSB_zns/36GB
+# 디바이스 설정
+DEV=nvme0n1
 
-DEV=nvme0n2
+for ZONE_VALUE in "${ZONE_VALUES[@]}"; do
+    export MAX_NR_OPEN_IO_ZONES=$ZONE_VALUE
+    export MAX_NR_ACTIVE_IO_ZONES=$ZONE_VALUE
 
-#$NOFINISH_LZ $FINISH_LZ
-do
+    echo "Running experiment with MAX_NR_OPEN_IO_ZONES=$MAX_NR_OPEN_IO_ZONES 
 
-for SCHEME in $FINISH_EZ  $NOFINISH_EZ 
-    for i in tmp
-    do
-        for WORKLOAD_TYPE in uniform
-        do  
-            echo "Running with fixed Open Zone Limit: $MAX_NR_OPEN_IO_ZONES"
+#and MAX_NR_ACTIVE_IO_ZONES=$MAX_NR_ACTIVE_IO_ZONES"
+    
+    # 결과 파일 경로 설정
+    RESULT_PATH=${RESULT_DIR_PATH}/zone${ZONE_VALUE}.txt
 
-            echo "${MAX_NR_OPEN_IO_ZONES}" > /home/jaewan/연구/ATC_testdata/tmp/openzone_value
+    if [ -f ${RESULT_PATH} ]; then
+        echo "Already exists: $RESULT_PATH"
+        sleep 5
+        continue
+    fi
 
-            if [ $SCHEME -eq $FINISH_EZ ]; then
-                RESULT_PATH=${RESULT_DIR_PATH}/${WORKLOAD_TYPE}_FINISH_EZ_${MAX_NR_OPEN_IO_ZONES}_${i}.txt
-                OPTIONS=/home/jaewan/연구/YCSB-cpp/rocksdb/zlsm_motivation.ini
-                sed -i "s/^  finish_scheme=.*/  finish_scheme=0/" $OPTIONS
-                sed -i "s/^  reset_scheme=.*/  reset_scheme=0/" $OPTIONS
-            # elif [ $SCHEME -eq $FINISH_LZ ]; then
-            #     RESULT_PATH=${RESULT_DIR_PATH}/${WORKLOAD_TYPE}_FINISH_LZ_${MAX_NR_OPEN_IO_ZONES}_${i}.txt
-            #     OPTIONS=/home/jaewan/연구/YCSB-cpp/rocksdb/zlsm_motivation.ini
-            #     sed -i "s/^  finish_scheme=.*/  finish_scheme=0/" $OPTIONS
-            #     sed -i "s/^  reset_scheme=.*/  reset_scheme=1/" $OPTIONS
-            elif [ $SCHEME -eq $NOFINISH_EZ ]; then
-                RESULT_PATH=${RESULT_DIR_PATH}/${WORKLOAD_TYPE}_NOFINISH_EZ_${MAX_NR_OPEN_IO_ZONES}_${i}.txt
-                OPTIONS=/home/jaewan/연구/YCSB-cpp/rocksdb/zlsm_motivation.ini
-                sed -i "s/^  finish_scheme=.*/  finish_scheme=1/" $OPTIONS
-                sed -i "s/^  reset_scheme=.*/  reset_scheme=0/" $OPTIONS
-            # elif [ $SCHEME -eq $NOFINISH_LZ ]; then
-            #     RESULT_PATH=${RESULT_DIR_PATH}/${WORKLOAD_TYPE}_NOFINISH_LZ_${MAX_NR_OPEN_IO_ZONES}_${i}.txt
-            #     OPTIONS=/home/jaewan/연구/YCSB-cpp/rocksdb/zlsm_motivation.ini
-            #     sed -i "s/^  finish_scheme=.*/  finish_scheme=1/" $OPTIONS
-            #     sed -i "s/^  reset_scheme=.*/  reset_scheme=1/" $OPTIONS
-            else  
-                echo "error"
-            fi
+    while : ; do
+        # 디바이스 초기화
+        sudo -E /home/jaewan/zone_reset_all 0 904 > /home/jaewan/ATC_testdata/tmp/reset_log
+        sudo rm -rf /home/jaewan/ATC_testdata/log
+        sudo mkdir -p /home/jaewan/ATC_testdata/log
+        echo "mq-deadline" | sudo tee /sys/block/${DEV}/queue/scheduler
 
-            if [ -f ${RESULT_PATH} ]; then
-                echo "already $RESULT_PATH exists"
-                sleep 5
-                continue
-            fi
+        # ZenFS 파일 시스템 초기화
+        sudo -E /home/jaewan/EZC/rocksdb/plugin/zenfs/util/zenfs mkfs --force --enable_gc \
+        --zbd=${DEV} --aux_path=/home/jaewan/ATC_testdata/log > /home/jaewan/ATC_testdata/tmp2
 
-            while : 
-            do
-                sudo /home/jaewan/zone_reset_all 0 904 > /home/jaewan/연구/ATC_testdata/tmp/reset_log
-                sudo rm -rf /home/jaewan/연구/ATC_testdata/log
-                sudo mkdir -p /home/jaewan/연구/ATC_testdata/log
-                echo "mq-deadline" | sudo tee /sys/block/${DEV}/queue/scheduler
-                
-                sudo /home/jaewan/연구/EZC/rocksdb/plugin/zenfs/util/zenfs mkfs --force --enable_gc \
-                --zbd=${DEV} --aux_path=/home/jaewan/연구/ATC_testdata/log > /home/jaewan/연구/ATC_testdata/tmp2
+        echo "Result path: ${RESULT_PATH}"
 
-                echo ${RESULT_PATH}
-                sudo cp ${OPTIONS} /home/jaewan/연구/ATC_testdata/log/zenfsoptions.ini
+        # YCSB 실행 및 결과 저장
+        sudo -E /home/jaewan/YCSB-cpp/ycsb -load -db rocksdb -P workloads/workload_uniform \
+        -P rocksdb/rocksdb.properties -s > ${RESULT_DIR_PATH}/tmp
 
-                sudo /home/jaewan/연구/YCSB-cpp/ycsb -load -db rocksdb -P workloads/workload_${WORKLOAD_TYPE} -P rocksdb/rocksdb.properties -s > ${RESULT_DIR_PATH}/tmp
-                
-                if grep -q "BLOCKING TIME(ms)" ${RESULT_DIR_PATH}/tmp; then
-                    cat ${RESULT_DIR_PATH}/tmp > ${RESULT_PATH}
-                    rm -rf ${RESULT_DIR_PATH}/tmp
-                    break
-                else
-                    cat ${RESULT_DIR_PATH}/tmp > ${RESULT_DIR_PATH}/failed
-                    sleep 5
-                    break
-                fi
-            done
-        done
+        if grep -q "BLOCKING TIME(ms)" ${RESULT_DIR_PATH}/tmp; then
+            cat ${RESULT_DIR_PATH}/tmp > ${RESULT_PATH}
+            rm -rf ${RESULT_DIR_PATH}/tmp
+            break
+        else
+            cat ${RESULT_DIR_PATH}/tmp > ${RESULT_DIR_PATH}/failed
+            sleep 5
+            break
+        fi
     done
 done
 
-echo "all done"
+echo "All experiments completed."
